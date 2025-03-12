787cd610d9f83760f750539c30233ac0abe98ae5
Andy Schneider <andy@schnoberts.com>
2025-03-12 11:28:24 +0000

    Add a standard set of warning suppression

---
+++ cores/arduino/mbed/platform/disable-warnings-start.h
@@ -0,0 +1,16 @@
+#pragma GCC diagnostic push
+#pragma GCC diagnostic ignored "-Wuninitialized"
+#pragma GCC diagnostic ignored "-Wunused-parameter"
+#pragma GCC diagnostic ignored "-Wunknown-pragmas"
+#pragma GCC diagnostic ignored "-Wunused-but-set-variable"
+#pragma GCC diagnostic ignored "-Wsign-compare"
+#pragma GCC diagnostic ignored "-Wimplicit-fallthrough"
+#pragma GCC diagnostic ignored "-Wunused-variable"
+#pragma GCC diagnostic ignored "-Wreturn-type"
+#pragma GCC diagnostic ignored "-Wunused-function"
+#if defined(__GNUC__) && !defined(__cplusplus)
+#pragma GCC diagnostic ignored "-Wimplicit-function-declaration"
+#pragma GCC diagnostic ignored "-Wdiscarded-qualifiers"
+#else
+#pragma GCC diagnostic ignored "-Wmissing-field-initializers"
+#endif
